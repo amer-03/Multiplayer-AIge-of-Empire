ffbfa64d953e69f5f97b8919841d951404009a1f
Xpert <ahmad.pc.saad@gmail.com>
2025-03-22 02:42:01 +0100

    test

--- network/fusion/c2/communicator.c
+++ network/fusion/c2/communicator.c
@@ -43,6 +43,22 @@ Communicator* init_communicator(int listener_port, int destination_port, const c
         return NULL;
     }
 
+    int recieve_buff = RCVBUF_SIZE;
+    if (setsockopt(comm->sockfd, SOL_SOCKET, SO_RCVBUF, &recieve_buff, sizeof(recieve_buff)) < 0) {
+        perror("setsockopt SO_RCVBUF failed");
+        close(comm->sockfd);
+        free(comm);
+        return NULL;
+    }
+
+    int send_buff = SNDBUF_SIZE;
+    if (setsockopt(comm->sockfd, SOL_SOCKET, SO_SNDBUF, &send_buff, sizeof(send_buff)) < 0) {
+        perror("setsockopt SO_SNDBUF failed");
+        close(comm->sockfd);
+        free(comm);
+        return NULL;
+    }
+
 
 
     //Receiving address
