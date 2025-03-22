ad5c8e2638b5bcdc58a30c4e42362908600b17ff
Xpert <ahmad.pc.saad@gmail.com>
2025-03-22 03:56:30 +0100

    push

--- network/fusion/ctest/main.c
+++ network/fusion/ctest/main.c
@@ -18,7 +18,7 @@ int main() {
     int external_total_sent = 0;
     
     int packetlost = 0;
-    
+
     while (1) {
         // Receive queries from both communicators
         char* internal_query = receive_packet(python_communicator);
@@ -26,36 +26,37 @@ int main() {
         
         // Count received packets
         if (internal_query != NULL) {
-            printf("[Rvd PACKET]: %s | [RvdIntPackets]: %d",internal_query, internal_total_recieve);
+            //printf("[Rvd PACKET]: %s | [RvdIntPackets]: %d",internal_query, internal_total_recieve);
             internal_total_recieve++;
-            if(atoi(internal_query) != internal_total_recieve){
+            if(atoi(internal_query) - packetlost != internal_total_recieve){
                 packetlost++;
             }
-            printf(" | Packet Loss : %d \n", packetlost);              
+            //printf(" | Packet Loss : %d \n", packetlost);              
         }
         if (external_query != NULL) {
             external_total_recieve++;
-            printf("[Rvd PACKET]: %s | [RvdExtPackets]: %d",external_query, external_total_recieve);
+            //printf("[Rvd PACKET]: %s | [RvdExtPackets]: %d",external_query, external_total_recieve);
             if(atoi(external_query) - packetlost != external_total_recieve){
                 packetlost++;             
             }
-            printf(" | Packet Loss : %d \n", packetlost); 
+            //printf(" | Packet Loss : %d \n", packetlost); 
         }
         
         // Process the external query if it exists
         if (external_query != NULL) {
             send_packet(python_communicator, external_query);
             internal_total_sent++;
-            printf("[SNT PACKET]: %s | [SntIntPackets]: %d \n",external_query, internal_total_sent);
+            //printf("[SNT PACKET]: %s | [SntIntPackets]: %d \n",external_query, internal_total_sent);
         }
         
         // Process the internal query if it exists
         if (internal_query != NULL) {
             send_packet(external_communicator, internal_query);
             external_total_sent++;
-            printf("[SNT PACKET]: %s | [SntExtPackets]: %d \n",internal_query, external_total_sent);
+            //printf("[SNT PACKET]: %s | [SntExtPackets]: %d \n",internal_query, external_total_sent);
         }
-        
+
+        if(packetlost) printf("LOSS : %d\n", packetlost);
         usleep(SLEEP_TIME);
     }
     
