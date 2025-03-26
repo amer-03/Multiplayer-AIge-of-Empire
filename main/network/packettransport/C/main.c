161d5c5a69b6c6e0c98927ad2d0ed7132316de6f
Xpert <ahmad.pc.saad@gmail.com>
2025-03-26 08:46:36 +0100

    added players count

--- main/network/packettransport/C/main.c
+++ main/network/packettransport/C/main.c
@@ -83,7 +83,7 @@ int main() {
                 if(internal_packet.query[0] == 'R'){
                     char* request_buffer = malloc(BUFFER_SIZE * sizeof(char));
                     buffer = construct_buffer(discovery_communicator, internal_packet.query);
-                    snprintf(request_buffer, BUFFER_SIZE, "%s:%d", buffer, GAME_PORT);
+                    snprintf(request_buffer, BUFFER_SIZE, "%s:%d:%d", buffer, GAME_PORT, players_table->count);
                     send_discovery_broadcast(discovery_communicator, request_buffer);
                     free(request_buffer);  // Free the temporary buffer
                 } else {
